1da95da5ed27105d3ce1a2c845631cc0817220cb
JayvicWen <564311873@qq.com>
2015-09-26 18:40:43 +0800

    Add util for rsa decrypt when p and q are known. Override unhex in pwntools for odd length string.

--- Scripts/utils/utils.py
+++ Scripts/utils/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding:utf-8
+# encoding:utf-8
 
 import os
 import signal
@@ -8,15 +8,24 @@ import pwn
 from pwn import *
 
 __all__ = [
-'factor', 'gcd', 'ext_euclid',
-'set_pad_char', 'nops', 'left_pad', 'right_pad',
-'debug',
-] + [i for i in dir(pwn) if not i.startswith('__')] # export all imported from pwn
+    'factor', 'gcd', 'ext_euclid',
+    'set_pad_char', 'nops', 'left_pad', 'right_pad',
+    'debug',
+] + [i for i in dir(pwn) if not i.startswith('__')]  # export all imported from pwn
 
 
 #############################
 ### utils for calculation ###
 #############################
+
+def unhex(s):
+    """
+    Override unhex in pwntools.
+    Hex-strings with odd length are acceptable.
+    """
+    s = str(s).strip()
+    return (len(s) % 2 and '0' + s or s).decode('hex')
+
 
 def factor(n):
     """分解质因数"""
@@ -32,11 +41,13 @@ def factor(n):
         i += 2
     print n
 
+
 def gcd(a, b):
     """最大公约数，a > b"""
     if b == 1:
         return a
     return gcd(b, a % b)
+
 
 def ext_euclid(a, b):
     """扩展的欧几里德，a > b，ax+by=GCD(a, b) => x,y"""
@@ -46,22 +57,41 @@ def ext_euclid(a, b):
     return y, x - a / b * y
 
 
+def rsa_decrypt(c, e, p, q):
+    """Decrypt RSA encrypted message when p and q are known."""
+    # First calculate d.
+    phi_n = (p - 1) * (q - 1)
+    d, _ = ext_euclid(e, phi_n)
+    d %= phi_n
+
+    # Decrypt message using e.
+    c_value = int(enhex(c), 16)
+    n = p * q
+    m = pow(c_value, d, n)
+    m_hex = '%x' % m
+    return unhex(m_hex)
+
+
 #############################
 ### utils for EXP writing ###
 #############################
 
 _pad_char = '\x90'
 
+
 def set_pad_char(c):
     global _pad_char
     _pad_char = c
 
+
 def nops(n):
     return _pad_char * n
+
 
 def left_pad(s, n):
     assert len(s) <= n
     return nops(n - len(s)) + s
+
 
 def right_pad(s, n):
     assert len(s) <= n
@@ -81,19 +111,23 @@ def debug(args, shell=False, executable=None, cwd=None, env=None, timeout=pwnlib
     io.sendline('set prompt {0} '.format(term.text.bold_red('gdb$')))
     return io
 
+
 def _gdb_break(self, addr):
     if type(addr) == int or type(addr) == long:
         self.sendline('b *0x{0:x}'.format(addr))
     else:
         self.sendline('b {0}'.format(addr))
 
+
 def _gdb_run(self):
     self.sendline('r')
+
 
 def _gdb_continue(self):
     self.sendline('c')
 
-def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
+
+def _ext_interactive(self, prompt=term.text.bold_red('$') + ' '):
     """interactive(prompt = pwnlib.term.text.bold_red('$') + ' ')
     Does simultaneous reading and writing to the tube. In principle this just
     connects the tube to standard in and standard out, but in practice this
@@ -110,10 +144,11 @@ def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
     log.info('Switching to extensive interactive mode')
 
     go = threading.Event()
+
     def recv_thread():
         while not go.isSet():
             try:
-                cur = self.recv(timeout = 0.05)
+                cur = self.recv(timeout=0.05)
                 if cur:
                     sys.stderr.write(cur)
                     sys.stderr.flush()
@@ -121,7 +156,7 @@ def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
                 log.info('Got EOF while reading in interactive')
                 break
 
-    t = context.Thread(target = recv_thread)
+    t = context.Thread(target=recv_thread)
     t.daemon = True
     t.start()
 
@@ -129,9 +164,9 @@ def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
         while not go.isSet():
             if term.term_mode:
                 if self.debug_mode:
-                    data = term.readline.readline(prompt = '', float = True)
+                    data = term.readline.readline(prompt='', float=True)
                 else:
-                    data = term.readline.readline(prompt = prompt, float = True)
+                    data = term.readline.readline(prompt=prompt, float=True)
             else:
                 data = sys.stdin.readline()
 
@@ -141,7 +176,8 @@ def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
                     if data.strip() == 'c!':
                         data = 'c\n'
                         go.set()
-                    data = safeeval.const('"""{0}"""'.format(data.replace('"', r'\"')))
+                    data = safeeval.const(
+                        '"""{0}"""'.format(data.replace('"', r'\"')))
                     self.send(data)
                 except ValueError:
                     log.warning('Illegal input, ignored!')
@@ -155,7 +191,7 @@ def _ext_interactive(self, prompt = term.text.bold_red('$') + ' '):
         go.set()
 
     while t.is_alive():
-        t.join(timeout = 0.1)
+        t.join(timeout=0.1)
 
     signal.signal(signal.SIGINT, old_handler)
 
